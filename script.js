0eba7ae17bf8d05eb28c8cb0cf54fbcd17e2581e
K SAI PRANEETH REDDY <127538019+PraneethReddy15@users.noreply.github.com>
2024-02-25 16:39:02 +0530

    Update script.js

--- script.js
+++ script.js
@@ -11,3 +11,32 @@ function opentab(tabname) {
     event.currentTarget.classList.add("active-link");
     document.getElementById(tabname).classList.add("active-tab");
 }
+// -------------
+var sidemenu=document.getElementById("sidemenu");
+function openmenu()
+{
+    sidemenu.style.right="0";
+}
+function closemenu()
+{
+    sidemenu.style.right="-200px";
+
+}
+// -------------- contact------------------
+const scriptURL = 'https://script.google.com/macros/s/AKfycbz390uXzIrD2dHtXGM8JrxSwp8oRXyfCKmE-WZdLLG_BzeFt8IyabKayoHJsr1Pdczy-g/exec'
+const form = document.forms['submit-to-google-sheet']
+const submitmsg=document.getElementById("submit-msg")
+
+form.addEventListener('submit', e => {
+  e.preventDefault()
+  fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+    .then(response => {
+        submitmsg.innerHTML="Message sent successfully!!";
+        setTimeout(function()
+        {
+            submitmsg.innerHTML='';
+        },4000)
+        form.reset();
+    })
+    .catch(error => console.error('Error!', error.message))
+})
