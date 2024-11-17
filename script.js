2fff28d9587dcb02b7b41f85fb176730898b23dd
K SAI PRANEETH REDDY <127538019+PraneethReddy15@users.noreply.github.com>
2024-11-17 10:26:20 +0530

    Update script.js

--- script.js
+++ script.js
@@ -80,36 +80,3 @@ function closemenu()
     sidemenu.style.right="-200px";
 
 }
-// -------------- contact------------------
-const scriptURL = 'https://script.google.com/macros/s/AKfycbwP1zv15mAuQm2AgTAyrbysDXs7Dr8mH7z1jTFfzA8KkHunLO1rkIK5DAxhL06YFTeA1Q/exec'
-const form = document.forms['submit-to-google-sheet']
-const submitmsg=document.getElementById("submit-msg")
-
-form.addEventListener('submit', e => {
-  e.preventDefault()
-  fetch(scriptURL, { method: 'POST', body: new FormData(form)})
-    .then(response => {
-        submitmsg.innerHTML="Message sent successfully!!";
-        setTimeout(function()
-        {
-            submitmsg.innerHTML='';
-        },4000)
-        form.reset();
-    })
-    .catch(error => console.error('Error!', error.message))
-})
-// ------------------------
-var icon =document.getElementById("icon");
-icon.onclick=function()
-{
-    document.body.classList.toggle("light-theme");
-    if(document.body.classList.contains("light-theme"))
-    {
-        icon.src="./files/sun.png"
-    }
-    else{
-        icon.src="./files/moon.png"
-
-    }
-}
-// ----------------------
